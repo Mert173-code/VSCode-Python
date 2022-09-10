6223323e72530797efada07359d3dbcd5ae616df
Mert173-code <mertlale2003@hotmail.com>
2022-09-10 12:42:52 +0300

    01112021-1504

---
+++ 01_python_giris/0118_degiskenler.py
@@ -0,0 +1,19 @@
+#region değişken tanımlama
+# name="JHON"
+# print(name)
+#endregion
+"""
+#1- camel casing deve notasyonu
+okulNumarasi = 1001
+ad = "aziz"
+print(okulNumarasi,ad)
+
+#2- underscore casing  alt tire notasyonu
+okul_numarası =1001
+print(okul_numarası)
+"""
+import keyword
+print(keyword.kwlist)
+"""
+
+"""
---
+++ 01_python_giris/0119_concat.py
@@ -0,0 +1,19 @@
+# ver = "3.10.0"
+# os="10"
+# print("windows versiyonu"+ os + "python versiyonu "+ ver)
+
+# from msvcrt import kbhit
+
+
+# kullaniciAdı="İrem"
+# print("Hoş geldin\t" +   kullaniciAdı)
+
+
+"""
+Bugün günlerden Cumartesi, İstanbulda hava sıcaklığı 29 derece.
+"""
+derece= "29" 
+gun = "Cumartesi"
+konum = "İstanbul"
+
+print("Bugün günlerden " +gun +konum +"da hava sıcaklığı"+derece +"derece.")
