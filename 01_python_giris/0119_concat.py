0c82669a739d9e2ddfb89cbc69b750f84a0a3cfe
Mert173-code <mertlale2003@hotmail.com>
2022-09-10 12:54:46 +0300

    01112021-1504

--- 01_python_giris/0119_concat.py
+++ 01_python_giris/0119_concat.py
@@ -12,8 +12,15 @@
 """
 Bugün günlerden Cumartesi, İstanbulda hava sıcaklığı 29 derece.
 """
-derece= "29" 
-gun = "Cumartesi"
-konum = "İstanbul"
+# derece= "29" 
+# gun = "Cumartesi"
+# konum = "İstanbul"
 
-print("Bugün günlerden " +gun +konum +"da hava sıcaklığı"+derece +"derece.")
+# print("Bugün günlerden " +gun +" " +konum +"da hava sıcaklığı "  "+derece +" derece.")
+# print("Bugün günlerden " +gun +", " +konum +"da hava sıcaklığı "+derece +" derece.")
+
+# 6 fallowers .   0 fallowing
+# takıpEden="6"
+# takipEttiklerim="0"
+
+# print(takıpEden +  " " ,"fallowers ." + takipEttiklerim + " " ,"fallowing")
