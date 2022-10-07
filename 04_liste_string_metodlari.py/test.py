6ae68b77587ac4b559f75bd4d83de3aa1f4af24f
Mert173-code <mertlale2003@hotmail.com>
2022-10-07 21:25:56 +0300

    commit

---
+++ 04_liste_string_metodlari.py/test.py
@@ -0,0 +1,44 @@
+#ödev:
+"""
+rakamlar=[6,2,7,3,5]
+çiktı:
+["Altı","İki","Yedi","Üç","Beş" ]
+"""
+
+
+rakamlar=[8,7,6]
+for i in rakamlar:
+    if i==1:
+        print("BİR",end=" ")
+    elif i==2:
+        print("İKİ",end=" ")
+    elif i==3:
+        print("ÜÇ",end=" ")
+    elif i==4:
+        print("DORT",end=" ")
+    elif i==5:
+        print("BEŞ",end=" ")
+    elif i==6:
+        print("ALTI",end=" ")
+    elif i==7:
+        print("YEDİ",end=" ")
+    elif i==8:
+        print("SEKİZ",end=" ")
+    elif i==9:
+        print("DOKUZ",end=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
